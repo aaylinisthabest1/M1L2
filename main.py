a2f794f7e1786c12e5b069b50eea9b58d9130c44
aaylinisthabest1 <162820439+aaylinisthabest1@users.noreply.github.com>
2024-04-03 20:57:46 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import discord
+import random
 from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='$', intents=intents)
+bot = discord.Bot()
 
 @bot.event
 async def on_ready():
@@ -17,6 +19,26 @@ async def hello(ctx):
 @bot.command()
 async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
+
+@bot.command()
+async def roll(ctx):
+    roll_result = random.randint(1, 6)
+    await ctx.send(f"Your dice rolled a {roll_result}!")
+
+@bot.command()
+async def start(ctx):
+    print('Bot is online!')
+
+@bot.command()
+async def joke(ctx):
+    шутки = [
+        "Почему учёные не доверяют атомам? Потому что они составляют всё!",
+        "Я сказал моей жене, что она рисует брови слишком высоко. Она выглядела удивленной.",
+        "Почему пугало получило награду? Потому что оно было выдающимся в своей области!",
+        "Я читаю книгу о антигравитации. Её невозможно отложить!",
+        "Как называется фальшивая спагетти? Импаста!"
+    ]
+    await ctx.send(random.choice(шутки))
 
 @bot.group()
 async def cool(ctx):
